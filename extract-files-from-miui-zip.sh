c7fabdfeac979447198da4e32786d1de651e93b1
alterbang <alterbang@gmail.com>
2016-01-12 17:14:52 +0100

    complete device tree v1.0

---
+++ extract-files-from-miui-zip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+unzip miui.zip -d miuizip
+
+VENDOR=xiaomi
+DEVICE=libra
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    cp miuizip/system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
