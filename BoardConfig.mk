8be00ad1c4d96e90a8d4c1a8689a6ded16927181
alterbang <alterbang@gmail.com>
2016-01-27 19:10:34 +0100

    fixed bootimage building (xiaomi kernel also needs permissive)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,10 +88,17 @@ BOARD_USE_LEGACY_UI := true
 BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/libra/mkbootimg.mk
 #TARGET_KERNEL_SOURCE := kernel/xiaomi/libra
 #TARGET_KERNEL_CONFIG := msm8994-perf_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 ramoops_memreserve=2M
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 ramoops_memreserve=2M androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0000000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100 --dt device/xiaomi/libra/dt.img
+
+# >> alterbang
+# old: BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0000000 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --dt device/xiaomi/libra/dt.img
+BOARD_KERNEL_OFFSET = 0x0
+BOARD_KERNEL_TAGS_OFFSET := 0x0100
+BOARD_RAMDISK_OFFSET     := 0x02000000
+INSTALLED_DTIMAGE_TARGET := device/xiaomi/libra/dt.img
+# << alterbang
 
 #TARGET_PREBUILT_KERNEL := device/xiaomi/libra/kernel
 BOARD_KERNEL_SEPARATED_DT := true
@@ -101,6 +108,8 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 #BOARD_KERNEL_PAGESIZE    := 4096
 #BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 #BOARD_RAMDISK_OFFSET     := 0x02000000
+
+
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
@@ -228,5 +237,5 @@ BOARD_SEPOLICY_DIRS += device/xiaomi/libra/sepolicy
 
 BOARD_SEPOLICY_UNION += \
 
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+## Include path
+#TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
